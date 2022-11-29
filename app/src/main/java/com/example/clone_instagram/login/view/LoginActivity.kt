cc0777998b326237b7ed3710e4560931082fc42c
unknown <testpedroca@gmail.com>
2022-11-29 20:08:53 -0300

    Validação de formulário

--- app/src/main/java/com/example/clone_instagram/login/view/LoginActivity.kt
+++ app/src/main/java/com/example/clone_instagram/login/view/LoginActivity.kt
@@ -3,10 +3,14 @@ package com.example.clone_instagram.login.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.clone_instagram.R
+import com.google.android.material.textfield.TextInputLayout
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        findViewById<TextInputLayout>(R.id.login_edit_email_input)
+            .error = "Esse email é inválido"
     }
 }
