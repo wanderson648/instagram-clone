f521bd1e53d98a3b34c226baf135e3a0f8dd69ed
unknown <testpedroca@gmail.com>
2022-11-30 17:17:15 -0300

    Fragmento e Tela de Cadastro com Email

---
+++ app/src/main/java/com/example/clone_instagram/register/view/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.example.clone_instagram.register.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.clone_instagram.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
---
+++ app/src/main/java/com/example/clone_instagram/register/view/RegisterEmailFragment.kt
@@ -0,0 +1,19 @@
+package com.example.clone_instagram.register.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.clone_instagram.R
+
+class RegisterEmailFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_register_email, container, false)
+    }
+}
