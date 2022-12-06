43cc62022923c3c310eccc100869962743fdcacc
unknown <testpedroca@gmail.com>
2022-12-05 23:50:43 -0300

    Tela: Perfil de usuário

---
+++ app/src/main/java/com/example/clone_instagram/profile/view/FragmentProfile.kt
@@ -0,0 +1,19 @@
+package com.example.clone_instagram.profile.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.clone_instagram.R
+
+class FragmentProfile : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_profile, container, false)
+    }
+}
