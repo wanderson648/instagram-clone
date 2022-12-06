64187da39d30db8f987027884516f9d7a57f4dfd
unknown <testpedroca@gmail.com>
2022-12-06 18:10:54 -0300

    Barra de Status Programaticamente

--- app/src/main/java/com/example/clone_instagram/main/view/MainActivity.kt
+++ app/src/main/java/com/example/clone_instagram/main/view/MainActivity.kt
@@ -2,11 +2,15 @@ package com.example.clone_instagram.main.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat
 import com.example.clone_instagram.R
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        window.statusBarColor = ContextCompat.getColor(this, R.color.gray_text)
     }
 }
