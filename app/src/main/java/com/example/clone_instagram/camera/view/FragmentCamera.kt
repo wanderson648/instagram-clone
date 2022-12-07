b8fc70bdc17834e77afa64e3508a20a2a57dd488
unknown <testpedroca@gmail.com>
2022-12-06 23:27:36 -0300

    Tela de upload de foto pela galeria

---
+++ app/src/main/java/com/example/clone_instagram/camera/view/FragmentCamera.kt
@@ -0,0 +1,22 @@
+package com.example.clone_instagram.camera.view
+
+import android.os.Bundle
+import android.view.*
+import android.widget.ImageView
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.clone_instagram.R
+
+class FragmentCamera : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_camera, container, false)
+    }
+
+}
